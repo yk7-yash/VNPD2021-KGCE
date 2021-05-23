a1ba90c4cfb001dfc4e6aa9f536330d8df75de2b
Yash Khavnekar <48960540+yk7-yash@users.noreply.github.com>
2021-05-23 16:27:18 +0530

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -1,1 +1,19 @@
-
+def print_factors(x):
+   print("The factors of",x,"are:")
+   arr=[]
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+           if (i % 2==0):
+             arr.append(i)
+   print(arr)
+   result=1
+   for a in arr:
+         result = result * a
+   print(result)
+   
+num = int(input())
+if (num<10):
+  print("Invalid Number")
+else:
+  print_factors(num)
